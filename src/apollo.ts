67a3c9d60c8f6c939837acaf49a9b8a400c9d816
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-08-08 17:17:45 +0300

    #2.3 Apollo Setup part Three

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,40 @@
 import ApolloClient, { Operation } from "apollo-boost";
 
 const client = new ApolloClient({
+  clientState: {
+    defaults: {
+      auth: {
+        __typename: "Auth",
+        isLoggedIn: Boolean(localStorage.getItem("jwt"))
+      }
+    },
+    resolvers: {
+      Mutation: {
+        logUserIn: (_, { token }, { cache }) => {
+          localStorage.setItem("jwt", token);
+          cache.writeData({
+            data: {
+              auth: {
+                __typename: "Auth",
+                isLoggedIn: true
+              }
+            }
+          });
+          return null;
+        },
+        logUserOut: (_, __, { cache }) => {
+          localStorage.removeItem("jwt");
+          cache.writeData({
+            data: {
+              __typename: "Auth",
+              isLoggedIn: false
+            }
+          });
+          return null;
+        }
+      }
+    }
+  },
   request: async (operation: Operation) => {
     operation.setContext({
       headers: {
