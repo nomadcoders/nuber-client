1b570083f5dcad34fc2f2956c6cef7e135a2aac6
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-08-08 16:57:41 +0300

    #2.2 Apollo Setup part Two

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,13 @@
-import ApolloClient from "apollo-boost";
+import ApolloClient, { Operation } from "apollo-boost";
 
 const client = new ApolloClient({
+  request: async (operation: Operation) => {
+    operation.setContext({
+      headers: {
+        "X-JWT": localStorage.getItem("jwt") || ""
+      }
+    });
+  },
   uri: "http://localhost:4000/graphql"
 });
 
