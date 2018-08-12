2a9f0fc9f5678bf327de36eee90364288c2ea0cd
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-08-12 15:21:08 +0300

    #2.52 Geocoding part Two

--- src/Routes/FindAddress/FindAddressContainer.tsx
+++ src/Routes/FindAddress/FindAddressContainer.tsx
@@ -62,6 +62,7 @@ class FindAddressContainer extends React.Component<any, IState> {
         lng
       },
       disableDefaultUI: true,
+      minZoom: 8,
       zoom: 11
     };
     this.map = new maps.Map(mapNode, mapConfig);
@@ -85,9 +86,18 @@ class FindAddressContainer extends React.Component<any, IState> {
       [name]: value
     } as any);
   };
-  public onInputBlur = () => {
+  public onInputBlur = async () => {
     const { address } = this.state;
-    geoCode(address);
+    const result = await geoCode(address);
+    if (result !== false) {
+      const { lat, lng, formatted_address: formatedAddress } = result;
+      this.setState({
+        address: formatedAddress,
+        lat,
+        lng
+      });
+      this.map.panTo({ lat, lng });
+    }
   };
   public reverseGeocodeAddress = async (lat: number, lng: number) => {
     const reversedAddress = await reverseGeoCode(lat, lng);
--- src/mapHelpers.ts
+++ src/mapHelpers.ts
@@ -5,7 +5,20 @@ import { MAPS_KEY } from "./keys";
 export const geoCode = async (address: string) => {
   const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${MAPS_KEY}`;
   const { data } = await axios(URL);
-  console.log(data);
+  if (!data.error_message) {
+    const { results } = data;
+    const firstPlace = results[0];
+    const {
+      formatted_address,
+      geometry: {
+        location: { lat, lng }
+      }
+    } = firstPlace;
+    return { formatted_address, lat, lng };
+  } else {
+    toast.error(data.error_message);
+    return false;
+  }
 };
 
 export const reverseGeoCode = async (lat: number, lng: number) => {
@@ -14,6 +27,9 @@ export const reverseGeoCode = async (lat: number, lng: number) => {
   if (!data.error_message) {
     const { results } = data;
     const firstPlace = results[0];
+    if (!firstPlace) {
+      return false;
+    }
     const address = firstPlace.formatted_address;
     return address;
   } else {
